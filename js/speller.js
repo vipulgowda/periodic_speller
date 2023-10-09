2f9caac69f2052f240dca722e693fc00cbf257da
vipul <vipulgowda96@gmail.com>
2023-10-09 11:51:29 -0700

    Better algorithm implementation

--- js/speller.js
+++ js/speller.js
@@ -1,4 +1,5 @@
 var elements;
+var symbols = {};
 
 await loadPeriodicTable();
 
@@ -6,33 +7,77 @@ async function loadPeriodicTable() {
   elements = await fetch("periodic-table.json").then((response) =>
     response.json()
   );
+  for (let element of elements) {
+    symbols[element.symbol.toLowerCase()] = element;
+  }
 }
 
-function check(inputWord) {
-  // TODO: determine if `inputWord` can be spelled
-  // with periodic table symbols; return array with
-  // them if so (empty array otherwise)
-  const res = [];
-  for (let el = 0; el < inputWord.length; el++) {
-    if (elements.find((e) => e.symbol.toLowerCase()[0] === inputWord[el])) {
-      res.push(inputWord[el]);
+function findCandidates(inputWord) {
+  var oneLetterSymbols = [];
+  var twoLetterSymbols = [];
+
+  for (let i = 0; i < inputWord.length; i++) {
+    // collect one letter symbol options
+    if (inputWord[i] in symbols && !oneLetterSymbols.includes(inputWord[i])) {
+      oneLetterSymbols.push(inputWord[i]);
+    }
+
+    // collect two letter symbol options
+    if (i <= inputWord.length - 2) {
+      let two = inputWord.slice(i, i + 2);
+      if (two in symbols && !twoLetterSymbols.includes(two)) {
+        twoLetterSymbols.push(two);
+      }
     }
   }
-  if (res.length > 0) {
-    return res;
+
+  return [...twoLetterSymbols, ...oneLetterSymbols];
+}
+
+function spellWord(symbols, charsLeft) {
+  if (charsLeft.length == 0) {
+    return [];
   } else {
+    if (charsLeft.length >= 2) {
+      let doubleChar = charsLeft.slice(0, 2);
+      let rest = charsLeft.slice(2);
+      if (symbols.includes(doubleChar)) {
+        if (rest != "") {
+          let res = [doubleChar, ...spellWord(symbols, rest)];
+          if (res.join("") === charsLeft) {
+            return res;
+          }
+        } else {
+          return [doubleChar];
+        }
+      }
+    }
+    if (charsLeft.length >= 1) {
+      let singleChar = charsLeft[0];
+      let rest = charsLeft.slice(1);
+      if (symbols.includes(singleChar)) {
+        if (rest != "") {
+          let res = [singleChar, ...spellWord(symbols, rest)];
+          if (res.join("") === charsLeft) {
+            return res;
+          }
+        } else {
+          return [singleChar];
+        }
+      }
+    }
     return [];
   }
 }
 
-function lookup(elementSymbol) {
-  // TODO: return the element entry based on specified
-  // symbol (case-insensitive)
-  const resultEl = elements.find(
-    (el) => el.symbol.toLowerCase() === elementSymbol.toLowerCase()
-  );
-  debugger
-  return resultEl;
+function check(inputWord) {
+  const symbols = findCandidates(inputWord);
+  console.log(symbols, inputWord)
+  return spellWord(symbols, inputWord);
 }
 
-export default { check, lookup }
+function lookup(elementSymbol) {
+  return symbols[elementSymbol];
+}
+
+export default { check, lookup };
